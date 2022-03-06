4888582b0d7c9fdf47b753acaad552d8616b2189
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-03-06 12:51:00 -0800

    Added task 5.

---
+++ Lab2/task5.c
@@ -0,0 +1,56 @@
+#include "stdio.h"
+#include "fcntl.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+
+int CloseFile(FILE* file, char* errorText)
+{
+    // fclose() returns 0, if success.
+    if (fclose(file))
+    {
+        perror(errorText);
+        return 0;
+    }
+
+    return 1;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        perror("Invalid arguments amount!\nFirst argument - source file path.\nSecond argument - dest file path");
+        return 0;
+    }
+
+    int srcDescriptor;
+    if ((srcDescriptor = open(argv[1], O_RDONLY)) == -1)
+    {
+        perror("File error: Can't read source file or it doesn't exist.");
+        return 0;
+    }
+
+    int destDescriptor;
+    if ((destDescriptor = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0644)) == -1)
+    {
+        perror("File error: Can't open destination file.");
+        return 0;
+    }
+
+    struct stat srcFileStat;
+    fstat(srcDescriptor, &srcFileStat);
+    chmod(argv[2], srcFileStat.st_mode);
+
+    FILE* srcFile = fdopen(srcDescriptor, "r");
+    FILE* destFile = fdopen(destDescriptor, "w");
+
+    char currChar;
+    while ((currChar = getc(srcFile)) != EOF)
+        putc(currChar, destFile);
+
+    if (CloseFile(srcFile, "Error occurred while trying to close source file.") |
+        CloseFile(destFile, "Error occurred while trying to close destination file."))
+        return 0;
+
+    return 1;
+}
